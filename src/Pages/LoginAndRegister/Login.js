e0ca4cec3aa6da9109e0016baa05967ddd5a4f3d
Rana Arju <arjurana530@gmail.com>
2022-04-17 16:43:16 +0600

    login and register form design with full responsive

--- src/Pages/LoginAndRegister/Login.js
+++ src/Pages/LoginAndRegister/Login.js
@@ -1,10 +1,50 @@
 import React from 'react';
-
+import { Button, Container, Form } from 'react-bootstrap';
+import { Link } from 'react-router-dom';
+import { FcGoogle } from 'react-icons/fc';
+import { BsGithub } from 'react-icons/bs';
+import "./LoginAndRegister.css";
 const Login = () => {
     return (
-        <div>
-            <h2>login</h2>
+        <Container>
+            <div  className='form-box'>
+            <h2 className='text-center my-4 title'>Please Login</h2>
+            <Form>
+            <Form.Group className="mb-3" controlId="formBasicEmail">
+                <Form.Label>Email address</Form.Label>
+                <Form.Control type="email" placeholder="Enter email" />
+                <Form.Text className="text-muted">
+                We'll never share your email with anyone else.
+                </Form.Text>
+            </Form.Group>
+
+            <Form.Group className="mb-3" controlId="formBasicPassword">
+                <Form.Label>Password</Form.Label>
+                <Form.Control type="password" placeholder="Password" />
+            </Form.Group>
+            <Form.Group className="mb-3" controlId="formBasicCheckbox">
+                <p>New of Defency? <Link to="/register">Please Register</Link></p>
+            </Form.Group>
+            <Form.Group className="d-grid gap-2 mb-3">
+            <Button variant="primary" size="lg" type="submit">
+                Login
+            </Button>
+            </Form.Group>
+              <Form.Group className="mb-3" controlId="formBasicCheckbox">
+                <p>Forgot Password? <a href="/">Please Reset</a></p>
+            </Form.Group>
+            </Form>
+                <div className="v-line">
+                <div></div>
+                <p>or</p>
+                <div></div>
+                </div>
+              <div className="social-media">
+                <Button variant="primary" size="lg" ><span className='icon'><FcGoogle /></span>Github</Button>
+                <Button variant="primary" size="lg" ><span className='icon'><BsGithub /></span>Github</Button>
+            </div>
         </div>
+        </Container>
     );
 };
 
--- src/Pages/LoginAndRegister/Register.js
+++ src/Pages/LoginAndRegister/Register.js
@@ -1,10 +1,52 @@
 import React from 'react';
+import { Button, Container, Form } from 'react-bootstrap';
+import { Link } from 'react-router-dom';
+import { FcGoogle } from 'react-icons/fc';
+import { BsGithub } from 'react-icons/bs';
+import "./LoginAndRegister.css";
 
 const Register = () => {
     return (
-        <div>
-            <h2>Register</h2>
+          <Container>
+            <div  className='form-box'>
+            <h2 className='text-center my-4 title'>Please Register</h2>
+            <Form>
+            <Form.Group className="mb-3" controlId="formBasicEmail">
+                <Form.Label>Your Full Name</Form.Label>
+                <Form.Control type="text" placeholder="Enter Full Name" />
+            </Form.Group>
+            <Form.Group className="mb-3" controlId="formBasicEmail">
+                <Form.Label>Email address</Form.Label>
+                <Form.Control type="email" placeholder="Enter Email" />
+                <Form.Text className="text-muted">
+                We'll never share your email with anyone else.
+                </Form.Text>
+            </Form.Group>
+
+            <Form.Group className="mb-3" controlId="formBasicPassword">
+                <Form.Label>Password</Form.Label>
+                <Form.Control type="password" placeholder="Password" />
+            </Form.Group>
+            <Form.Group className="mb-3" controlId="formBasicCheckbox">
+                <p>Already have an Account? <Link to="/login">Please Login</Link></p>
+            </Form.Group>
+            <Form.Group className="d-grid gap-2">
+            <Button variant="primary" size="lg" type="submit">
+                Register
+            </Button>
+            </Form.Group>
+            </Form>
+                <div className="v-line">
+                <div></div>
+                <p>or</p>
+                <div></div>
+                </div>
+              <div className="social-media">
+                <Button variant="primary" size="lg" ><span className='icon'><FcGoogle /></span>Github</Button>
+                <Button variant="primary" size="lg" ><span className='icon'><BsGithub /></span>Github</Button>
+            </div>
         </div>
+        </Container>
     );
 };
 
