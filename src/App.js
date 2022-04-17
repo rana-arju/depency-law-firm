7458b1a846ac0bb1375aa9dde5ec1e6b6179df90
Rana Arju <arjurana530@gmail.com>
2022-04-17 11:16:41 +0600

    All Route Set

--- src/App.js
+++ src/App.js
@@ -1,23 +1,25 @@
-import logo from './logo.svg';
+import { Route, Routes } from 'react-router-dom';
+import Home from './Pages/Home/Home/Home';
+import About from './Pages/About/About';
+import Blog from './Pages/Blog/Blog';
+import Error404 from './Pages/Error/Error404';
+import Login from './Pages/LoginAndRegister/Login';
+import Register from './Pages/LoginAndRegister/Register';
+import Checkout from './Pages/Checkout/Checkout';
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+        <Routes>
+          <Route path='/' element={<Home />} />
+          <Route path='/about' element={<About />} />
+          <Route path='/blog' element={<Blog />} />
+          <Route path='*' element ={<Error404 />} />
+          <Route path='/login' element={<Login />} />
+          <Route path='/register' element={<Register />} />
+          <Route path='/checkout' element={<Checkout />} />
+        </Routes>
     </div>
   );
 }
