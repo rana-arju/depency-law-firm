3f1ccb1cccb8c34a0c850989e40effdc617be495
Rana Arju <arjurana530@gmail.com>
2022-04-17 13:19:50 +0600

    Footer Section add

--- src/Shared/Footer/Footer.js
+++ src/Shared/Footer/Footer.js
@@ -1,9 +1,13 @@
 import React from 'react';
-
+import "./Footer.css";
 const Footer = () => {
+    const day = new Date().getDate();
+    const month = new Date().getMonth()+1;
+    const year = new Date().getFullYear();
     return (
-        <div>
-            <h2>Thsis is Footer page</h2>
+        <div className='footer-section'>
+            <p>All Right Reserved &copy; Rana Arju ❤ </p>
+            <p>Today: <small>{day}/{month}/{year}</small></p>
         </div>
     );
 };
