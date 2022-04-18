fa1a7999f84bc03ab2e1b7c723ceef71a3eb1121
Rana Arju <arjurana530@gmail.com>
2022-04-18 20:34:04 +0600

    about page create and responsive full page

--- src/Pages/About/About.js
+++ src/Pages/About/About.js
@@ -8,9 +8,12 @@ const About = () => {
             <h2 className='title'>About me</h2>
             <div className="about-container">
                 <div className="about-left">
-                    <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Voluptates doloremque expedita consequatur officia, eaque dolorem pariatur optio eum incidunt dignissimos.</p>
+                    <p><strong>My Name is Rana Arju.</strong>
+                    i'm a student of cox's bazar polytechnic institute in computer department. my goal in 2022 is to become a web developer, for that's why i learned html, css, bootstrap, Javascript, react js and learning more. My final goal is to become a skilled programmer and software engineer. in march 2022, i joined the ICT olympiad as a co-leader of cox's bazar polytechnic  institute.  </p> 
+                    <p>i'm very dedicated to my work which is why i do other work including programming for 10 hours every day.</p>
+                  
                 </div>
-                <div className="about-left">
+                <div className="about-right">
                     <img src={img} alt="" />
                 </div>
             </div>
